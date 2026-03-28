6a80ba7f2f2a35b3b3398891d54e0d3ecdd46afb
lucyax <luciameslava@gmail.com>
2026-03-28 13:50:19 +0100

    print_utils added

---
+++ ft_printf_utils.c
@@ -0,0 +1,39 @@
+#include "ft_printf.h"
+
+/*ft_print_char
+** Imprime un solo caracter usando write()
+** Recibe int y no char porque va_arg promociona char a int automaticamente
+** Devuelve 1 prique siempre imprime exactemente 1 caracetr
+*/
+int	ft_print_char(int c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+/*ft_print_str
+** Imrpime un string char a char
+** Si el punero es NULL imprime "(null)" como hace el printf original
+** Devuelve el numero de caracteres impresos
+*/
+int	ft_printf_str(char *str)
+{
+	int	len;
+
+	if (!str)
+		str = "(null)";
+	len = ft_strlen(str);
+	write(1, str, len);
+	return (len);
+}
+
+/*ft_print_percent
+**Imprime carccter % literal
+** No consume argumento de va_list
+** Devuelve 1 porque imprime exactamente 1 char
+*/
+int	ft_print_percent(void)
+{
+	write(1, "%", 1);
+	return (1);
+}
